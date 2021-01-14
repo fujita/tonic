cd2656be18f7049710917a71795f990a91d6a4ca
FUJITA Tomonori <fujita.tomonori@gmail.com>
2021-01-14 15:34:33 +0900

    workaround for std::net::TcPListener using too small backlog (256)
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -2,6 +2,10 @@ use tonic::{transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
+use tokio::net::TcpListener;
+use socket2::{Domain, Socket, Type};
+use std::net::SocketAddr;
+use futures::TryFutureExt;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -25,14 +29,30 @@ impl Greeter for MyGreeter {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::]:50051".parse().unwrap();
+    let addr: SocketAddr = "[::]:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
     println!("GreeterServer listening on {}", addr);
 
+    // workaround for std::net::TcPListener using too small backlog (256)
+    let incoming = {
+            let sock = Socket::new(Domain::ipv6(), Type::stream(), None).unwrap();
+            sock.set_reuse_address(true).unwrap();
+            sock.set_reuse_port(true).unwrap();
+            sock.set_nonblocking(true).unwrap();
+            sock.bind(&addr.into()).unwrap();
+            sock.listen(8192).unwrap();
+            let l = TcpListener::from_std(sock.into_tcp_listener())?;
+            async_stream::stream! {
+                while let item = l.accept().map_ok(|(st,_)|st).await {
+                    yield item;
+                }
+            }
+        };
+
     Server::builder()
         .add_service(GreeterServer::new(greeter))
-        .serve(addr)
+        .serve_with_incoming(incoming)
         .await?;
 
     Ok(())
